3bef2064904e0d64d1efcdb8c95dcc62a2268c1c
Demerson Reis <demersonsilvadosreis@gmail.com>
2022-03-24 00:16:01 -0300

    primary

---
+++ backend/src/main/java/br/com/demerson/SpringFilmes/SpringFilmesApplication.java
@@ -0,0 +1,13 @@
+package br.com.demerson.SpringFilmes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFilmesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFilmesApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/demerson/SpringFilmes/SpringFilmesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.demerson.SpringFilmes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFilmesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
